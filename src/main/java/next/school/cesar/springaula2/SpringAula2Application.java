bbe777ce9c9de061372ab9bfcaafd319bd544e3b
GersonRS <grs@cesar.org.br>
2023-08-22 16:27:24 -0300

    Adicionando arquivos da aula 2 do modulo spring

---
+++ src/main/java/next/school/cesar/springaula2/SpringAula2Application.java
@@ -0,0 +1,13 @@
+package next.school.cesar.springaula2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAula2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAula2Application.class, args);
+	}
+
+}
---
+++ src/test/java/next/school/cesar/springaula2/SpringAula2ApplicationTests.java
@@ -0,0 +1,13 @@
+package next.school.cesar.springaula2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAula2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
